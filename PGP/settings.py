7a270b51537fdaaf1c80bccf7591b15a9e104e3b
Magoratogha <magoratoga@gmail.com>
2018-04-19 19:07:32 -0500

    Retoques

--- PGP/settings.py
+++ PGP/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'PGP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'PGP',
+        'USER': 'postgres',
+        'PASSWORD': '24315860',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
