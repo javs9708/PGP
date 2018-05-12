4aa16277105fbb4a6493e1d134033d5efae7b756
Alexander Villegas <villegassanchezjhonalexander@gmail.com>
2018-05-12 17:00:03 -0500

    Urls

--- apps/gestion/urls.py
+++ apps/gestion/urls.py
@@ -4,6 +4,9 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
 
-    url(r'^menu/',gestionMenu, name='menu')
+    url(r'^menu/',gestionMenu, name='menu'),
+    url(r'^cuentas/',gestionCuentas, name='cuentas'),
+    url(r'^transacciones/',gestionTransacciones, name='transacciones'),
+    url(r'^presupuesto/',gestionPresupuesto, name='presupuesto')
 
 ]
--- apps/gestion/views.py
+++ apps/gestion/views.py
@@ -16,3 +16,39 @@ def gestionMenu(request):
         	'usuario': usuario,
     }
     return HttpResponse(template.render(ctx,request))
+
+@login_required(login_url='/ingresar/')
+def gestionCuentas(request):
+    username = request.GET.get('username')
+    template = loader.get_template('gestion/gestionCuentas.html')
+    user = User.objects.filter(username=username)
+    usuario = Usuario.objects.filter(user_id=user[0].id)
+    usuario = usuario[0]
+    ctx = {
+        	'usuario': usuario,
+    }
+    return HttpResponse(template.render(ctx,request))
+
+@login_required(login_url='/ingresar/')
+def gestionTransacciones(request):
+    username = request.GET.get('username')
+    template = loader.get_template('gestion/gestionTransacciones.html')
+    user = User.objects.filter(username=username)
+    usuario = Usuario.objects.filter(user_id=user[0].id)
+    usuario = usuario[0]
+    ctx = {
+        	'usuario': usuario,
+    }
+    return HttpResponse(template.render(ctx,request))
+
+@login_required(login_url='/ingresar/')
+def gestionPresupuesto(request):
+    username = request.GET.get('username')
+    template = loader.get_template('gestion/gestionPresupuesto.html')
+    user = User.objects.filter(username=username)
+    usuario = Usuario.objects.filter(user_id=user[0].id)
+    usuario = usuario[0]
+    ctx = {
+        	'usuario': usuario,
+    }
+    return HttpResponse(template.render(ctx,request))
