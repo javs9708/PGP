8a5696668bf6aeac7408ff04d2d64674e8278df7
Magoratogha <magoratoga@gmail.com>
2018-05-17 17:32:44 -0500

    inicio template crear cuentas

--- PGP/settings.py
+++ PGP/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'PGP',
         'USER': 'postgres',
-        'PASSWORD': 'javs',
+        'PASSWORD': '24315860',
         'HOST': 'localhost',
         'PORT': '5432',
     }
