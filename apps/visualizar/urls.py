34ddae311030ae587372af70c74419b374bd7861
Alexander Villegas <villegassanchezjhonalexander@gmail.com>
2018-05-22 19:55:19 -0500

    avance

--- apps/visualizar/urls.py
+++ apps/visualizar/urls.py
@@ -5,4 +5,7 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
 
     url(r'^menu/',visualizarMenu, name='menu'),
+    url(r'^cuentas/',visualizarCuentas, name='cuentas'),
+    url(r'^transacciones/',visualizarTransacciones, name='transacciones'),
+    url(r'^presupuestos/',visualizarPresupuestos, name='presupuesto'),
 ]
--- apps/visualizar/views.py
+++ apps/visualizar/views.py
@@ -1,14 +1,64 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from ..usuario.models import Usuario
+from ..gestion.models import Tarjeta,Prestamo, Inversion, Chequera, Ingreso, Gasto, Transferencia, Presupuesto
 from django.contrib.auth.models import User
 from django.template import loader
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/ingresar/')
 def visualizarMenu(request):
-    username = request.GET.get('username')
     template = loader.get_template('visualizar/Visualizar.html')
+    if request.method == 'GET':
+        username = request.GET.get('username')    
+        user = User.objects.filter(username=username)
+        usuario = Usuario.objects.filter(user_id=user[0].id)
+        usuario = usuario[0]
+        ctx = {
+            	'usuario': usuario,
+        }
+        return HttpResponse(template.render(ctx,request))
+
+@login_required(login_url='/ingresar/')
+def visualizarCuentas(request):
+    template = loader.get_template('visualizar/visualizar_cuentas.html')
+    if request.method == 'GET':
+        username = request.GET.get('username')
+        user = User.objects.filter(username=username)
+        usuario = Usuario.objects.filter(user_id=user[0].id)
+        usuario = usuario[0]
+
+        tarjeta = Tarjeta.objects.filter(user_id=user[0].id)
+        prestamo = Prestamo.objects.filter(user_id=user[0].id)
+        inversion = Inversion.objects.filter(user_id=user[0].id)
+        chequera = Chequera.objects.filter(user_id=user[0].id)
+
+        ctx = {
+            	'usuario': usuario,
+
+                'tarjeta': tarjeta,
+                'prestamo': prestamo,
+                'inversion': inversion,
+                'chequera': chequera,
+        }
+        return HttpResponse(template.render(ctx,request))
+
+@login_required(login_url='/ingresar/')
+def visualizarTransacciones(request):
+    username = request.GET.get('username')
+    template = loader.get_template('visualizar/visualizar_transacciones.html')
+    user = User.objects.filter(username=username)
+    usuario = Usuario.objects.filter(user_id=user[0].id)
+    usuario = usuario[0]
+    ctx = {
+        	'usuario': usuario,
+    }
+    return HttpResponse(template.render(ctx,request))
+
+@login_required(login_url='/ingresar/')
+def visualizarPresupuestos(request):
+    username = request.GET.get('username')
+    template = loader.get_template('visualizar/visualizar_transacciones.html')
     user = User.objects.filter(username=username)
     usuario = Usuario.objects.filter(user_id=user[0].id)
     usuario = usuario[0]
