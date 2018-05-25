18a0d3468e759a409e37830059e68fc544f6bb58
Magoratogha <magoratoga@gmail.com>
2018-05-24 20:43:41 -0500

    resposive completo gestionar cuentas

--- PGP/settings.py
+++ PGP/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'PGP.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-"""
+
 DATABASES = {
 'default': {
   'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -95,7 +95,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
 
 
 
