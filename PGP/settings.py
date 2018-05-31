b017b397a408b3d3f00e364cbe87a11ea8b19d2e
Alexander Villegas <villegassanchezjhonalexander@gmail.com>
2018-05-31 00:15:35 -0500

    :)

--- PGP/settings.py
+++ PGP/settings.py
@@ -77,6 +77,7 @@ WSGI_APPLICATION = 'PGP.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+"""
 DATABASES = {
 'default': {
   'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -95,7 +96,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
+
 
 
 
